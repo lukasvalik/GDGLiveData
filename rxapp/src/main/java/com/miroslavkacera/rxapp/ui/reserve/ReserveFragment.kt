568f0190ccbd34f98ba4cee6b77f6d8e97ec9a12
mkacera <mkacera@sygic.com>
2018-09-11 10:11:58 +0200

    Provide up navigation functionality

--- rxapp/src/main/java/com/miroslavkacera/rxapp/ui/reserve/ReserveFragment.kt
+++ rxapp/src/main/java/com/miroslavkacera/rxapp/ui/reserve/ReserveFragment.kt
@@ -5,8 +5,11 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.miroslavkacera.rxapp.databinding.FragmentReserveBinding
 import com.miroslavkacera.rxapp.repository.HotelRepository
 import dagger.android.support.AndroidSupportInjection
@@ -17,9 +20,17 @@ class ReserveFragment : Fragment() {
     @Inject
     lateinit var repository: HotelRepository
 
+    private lateinit var navController: NavController
+
     override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -30,5 +41,21 @@ class ReserveFragment : Fragment() {
 
         binding.viewModel = viewModel
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Respond to the action bar's Up/Home button
+                return navController.navigateUp()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
